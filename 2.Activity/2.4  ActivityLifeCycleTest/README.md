eb3af9cbd21d43f747f6a4428bcc7acdcb8def29
sir <783607273@qq.com>
2018-03-17 22:08:59 +0800

    add 2.4 ActivityLifeCycleTest

---
+++ 2.Activity/2.4  ActivityLifeCycleTest/README.md
@@ -0,0 +1,10 @@
+# 2.4  ActivityLifeCycleTest
+
+通过启动另一个活动 (NormalActivity) 和对话框 (DialogActivity) 来展示 activity 的生命周期。
+
+![](https://ws1.sinaimg.cn/large/006tKfTcgy1fpg60a3c2uj30a10kr0td.jpg)
+
+![](https://ws3.sinaimg.cn/large/006tKfTcgy1fpg6249pc8j313d0c9gpc.jpg)
+
+
+
---
+++ 2.Activity/2.4  ActivityLifeCycleTest/app/src/main/java/com/fjnu/sir/activitytest/MainActivity.java
@@ -0,0 +1,73 @@
+package com.fjnu.sir.activitytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NormalActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DialogActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+}
